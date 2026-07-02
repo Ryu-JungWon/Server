4a122398124a27a0b81c3f285817f006bbe3ee7e
류정원 <ryuzy@kakao.com>
2026-07-02 15:24:27 +0900

    로그인 API 완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -25,7 +25,20 @@ export async function signup(req, res) {
 
 // 로그인
 export async function login(req, res) {
-
+    const { userid, password } = req.body
+    const user = await authRepository.findByUserid(userid)
+    if(user){
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if(!isValidPassword){
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인"})
+        }
+        const token = await createJwtToken(user.id)
+        res.status(200).json({ token, user })
+    }else{
+        if(!isValidPassword){
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인"})
+        }
+    }
 }
 
 // 로그인 유지
