7e722665de74bc88bbaaf2fc4c86427de0a46f04
류정원 <ryuzy@kakao.com>
2026-07-03 09:51:10 +0900

    로그인 유지 API 완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -40,7 +40,11 @@ export async function login(req, res) {
 
 // 로그인 유지
 export async function me(req, res) {
-
+    const user = await authRepository.findById(req.id)
+    if(!user){
+        return res.status(404).json({ message: "일치하는 사용자가 없음"})
+    }
+    res.status(200).json({ token: req.token, userid: user.userid })
 }
 
 async function createJwtToken(id) {
